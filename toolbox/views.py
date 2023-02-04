1c3ffae8ee80733133e1ddbb34450d4cd95d2776
ngfchl <ngfchl@126.com>
2023-02-04 23:29:32 +0800

    添加toml文件解析方法

--- toolbox/views.py
+++ toolbox/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+import toml as toml
+
 
 # Create your views here.
+
+def parse_token(cmd):
+    """从配置文件解析获取相关项目"""
+    data = toml.load('db/ptools.toml')
+    return data.get(cmd)

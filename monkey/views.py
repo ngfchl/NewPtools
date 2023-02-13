e4223a854dddd24e469aa0d6a7d48eb4ad870c48
ngfchl <ngfchl@126.com>
2023-02-13 13:40:31 +0800

    1. 修复油猴脚本接口token检测

--- monkey/views.py
+++ monkey/views.py
@@ -17,7 +17,8 @@ router = Router(tags=['monkey'])
 @router.get('get_site/{token}/{host}', response={200: WebSiteMonkeySchemaOut, codes_4xx: CommonMessage})
 def get_site_by_host(request, token: str, host: str):
     """根据油猴发来的站点host返回站点相关信息"""
-    check_token = toolbox.check_token('token')
+    logger.info(token)
+    check_token = toolbox.check_token(token)
     if len(token) > 0 and not check_token:
         return 401, {'msg': 'Token认证失败！', 'code': -1}
     # logger.info(url)
@@ -31,11 +32,11 @@ def get_site_by_host(request, token: str, host: str):
 @router.post('save_site/{token}', response=CommonMessage)
 def save_site_by_monkey(request, token: str, mysite: MySiteSchemaIn = Form(...)):
     try:
-        check_token = toolbox.check_token('token')
+        check_token = toolbox.check_token(token)
         if len(token) > 0 and not check_token:
             return 401, {'msg': 'Token认证失败！', 'code': -1}
         logger.info(mysite)
-        # print(site)
+        # logger.info(site)
         res_my_site = MySite.objects.update_or_create(site=mysite.site, defaults=mysite.dict())
         logger.info(res_my_site[1])
         # todo 结果保存后获取一次数据，并更新一次注册时间

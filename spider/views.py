9f9d9b52b95a3aaace2f521c9e9390f99731aba0
ngfchl <ngfchl@126.com>
2023-03-05 13:21:29 +0800

    修复unit3d获取HR

--- spider/views.py
+++ spider/views.py
@@ -1403,14 +1403,14 @@ class PtSpider:
                     hr = ''.join(
                         details_html.xpath(site.my_hr_rule)
                     ).replace('H&R:', '').strip()
-                    print(hr)
+                    logger.info(hr)
                     if site.url in [
                         'https://monikadesign.uk/',
                         'https://pt.hdpost.top/',
                         'https://reelflix.xyz/',
                     ]:
-                        print(hr)
-                        hr.replace('\n', '').replace('有效', '').replace(':', '').strip('/').strip()
+                        logger.info(hr)
+                        hr = hr.replace('\n', '').replace('有效', '').replace(':', '').strip('/').strip()
                     my_hr = hr if hr else '0'
                     logger.info(f'h&r: "{hr}" ,解析后：{my_hr}')
                     # 做种与下载信息

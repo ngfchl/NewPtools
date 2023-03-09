0b5efc345a23a71a72fe8ea750554d63c67862c6
ngfchl <ngfchl@126.com>
2023-03-09 17:28:54 +0800

    添加 任务列表，cron任务列表，以及cron任务添加接口

--- schedule/views.py
+++ schedule/views.py
@@ -1,9 +1,12 @@
 import logging
+from typing import List
 
-from django_celery_beat.models import IntervalSchedule, PeriodicTask
+import pytz
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
 from ninja import Router
 
-from schedule import tasks
+from schedule.models import Task
+from schedule.schema import TaskSchemaOut, CrontabTaskSchemaIn, PeriodicTaskSchemaOut
 from toolbox.schema import CommonResponse
 
 # Create your views here.
@@ -11,25 +14,41 @@ logger = logging.getLogger('ptools')
 router = Router(tags=['schedule'])
 
 
-@router.get('/schedules', response=CommonResponse, description='定时列表')
+@router.get('/schedules', response=CommonResponse[List[TaskSchemaOut]], description='定时列表')
 def get_schedule_list(request):
-    data = [{
-        'task': f'schedule.tasks.{key}',
-        'desc': getattr(tasks, key).__doc__.strip()
-    } for key in dir(tasks) if key.startswith('auto_')]
+    data = [{'task': task, 'desc': desc} for (task, desc) in Task.choices]
     return CommonResponse.success(data=data)
 
 
-@router.get('/mysite', response=CommonResponse, description='我的站点-列表')
-def add_interval_schedule(request):
+@router.get('/crontab', response=CommonResponse[List[PeriodicTaskSchemaOut]], description='定时列表')
+def get_crontab_schedule_list(request):
+    data = PeriodicTask.objects.all()
+    return CommonResponse.success(data=list(data))
+
+
+@router.post('/add', response=CommonResponse, description='我的站点-列表')
+def add_crontab_schedule(request, task: CrontabTaskSchemaIn):
     # 创建一个时间调度器
-    schedule, created = IntervalSchedule.objects.get_or_create(
-        every=10,
-        period=IntervalSchedule.SECONDS
+    # schedule, _ = IntervalSchedule.objects.get_or_create(
+    #     every=10,
+    #     period=IntervalSchedule.SECONDS
+    # )
+    # 创建一个时间调度任务,无args即为无参数任务
+    # PeriodicTask.objects.create(
+    #     interval=schedule,  # 调度方式
+    #     name='',
+    #     task='schedule.tasks.*',
+    #     # args=json.dumps('args')
+    # )
+    # times = '0 4 * * *'
+    # params = ['minute', 'hour', 'day_of_week', 'day_of_month', 'month_of_year']
+    # crontab = {param: time for time, param in zip(times.split(' '), params)}
+    time_zone = pytz.timezone('Asia/Shanghai')
+    cron_schedule, _ = CrontabSchedule.objects.get_or_create(task.crontab, timezone=time_zone)
+    periodic_task = PeriodicTask.objects.create(
+        crontab=cron_schedule,
+        name=task.name,
+        task=task.task
     )
-    # 创建一个无参数的时间调度任务
-    PeriodicTask.objects.create(
-        interval=schedule,  # 调度方式
-        name='',
-        task=''
-    )
+    logger.info(periodic_task)
+    return CommonResponse.success(msg=f'{periodic_task.name} 任务创建成功！')

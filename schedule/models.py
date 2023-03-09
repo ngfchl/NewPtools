e99d0f9daffff632d1a0f54f06a0b60c19b3534f
ngfchl <ngfchl@126.com>
2023-03-09 17:27:47 +0800

    添加 任务列表

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from auxiliary.base import BaseEntity
+
+
 # Create your models here.
+class Task(models.TextChoices):
+    # 已实现的自动任务
+    auto_sign_in = 'schedule.tasks.auto_sign_in', '执行签到'
+    auto_get_status = 'schedule.tasks.auto_get_status', '更新个人数据'
+    auto_get_torrents = 'schedule.tasks.auto_get_torrents', '拉取最新种子'
+    # auto_program_upgrade = 'schedule.tasks.auto_program_upgrade', '程序更新'
+    # auto_get_torrent_hash = 'schedule.tasks.auto_get_torrent_hash', '自动获取种子HASH'
+    # auto_push_to_downloader = 'schedule.tasks.auto_push_to_downloader', '推送到下载器'
+    auto_update_license = 'schedule.tasks.auto_update_license', 'auto_update_license'
+    # auto_remove_expire_torrents = 'schedule.tasks.auto_remove_expire_torrents', '删除过期种子'

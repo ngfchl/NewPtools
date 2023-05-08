dec7ce1f525148c6466b1c15e693b9eba8b8655d
ngfchl <ngfchl@126.com>
2023-05-08 19:56:23 +0800

    添加自动任务 自动RSS解析

--- schedule/models.py
+++ schedule/models.py
@@ -15,3 +15,4 @@ class Task(models.TextChoices):
     # auto_push_to_downloader = 'schedule.tasks.auto_push_to_downloader', '推送到下载器'
     auto_update_license = 'schedule.tasks.auto_update_license', 'auto_update_license'
     # auto_remove_expire_torrents = 'schedule.tasks.auto_remove_expire_torrents', '删除过期种子'
+    auto_get_rss = 'schedule.tasks.auto_get_rss', 'auto_get_rss'

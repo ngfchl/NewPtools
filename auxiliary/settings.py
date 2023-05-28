228b6d3d7f62d193e111a40237bbb3493fb419c2
ngfchl <ngfchl@126.com>
2023-05-29 01:24:06 +0800

    手机项目静态资源

--- auxiliary/settings.py
+++ auxiliary/settings.py
@@ -162,10 +162,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(os.path.join(BASE_DIR, 'static')),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(os.path.join(BASE_DIR, 'static')),
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

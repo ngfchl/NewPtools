63399380b52a99a34dbe76789790fc72c81883e4
ngfchl <ngfchl@126.com>
2023-06-07 15:32:18 +0800

    隐藏尚未完善的自动任务

--- schedule/models.py
+++ schedule/models.py
@@ -11,7 +11,7 @@ class Task(models.TextChoices):
     auto_get_status = 'schedule.tasks.auto_get_status', '更新个人数据'
     auto_get_rss = 'schedule.tasks.auto_get_rss', 'RSS刷流'
     auto_get_torrents = 'schedule.tasks.auto_get_torrents', 'Free刷流'
-    auto_push_to_downloader = 'schedule.tasks.auto_push_to_downloader', '推送种子'
+    # auto_push_to_downloader = 'schedule.tasks.auto_push_to_downloader', '推送种子'
     # auto_torrents_package_files = 'schedule.tasks.auto_torrents_package_files', '拆包任务'
     auto_remove_brush_task = 'schedule.tasks.auto_remove_brush_task', '删种任务'
     auto_cleanup_not_registered = 'schedule.tasks.auto_cleanup_not_registered', '清理废弃种子-刷流'

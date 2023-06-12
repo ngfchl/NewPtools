2c00e7a482c76123cb0b03f6e5cba6cd439a94a9
ngfchl <ngfchl@126.com>
2023-06-12 11:21:47 +0800

    更新前端代码，去除自定义站点名称的检测

---
+++ templates/assets/index-54cc8c89.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index-17bc3907.js";import{H as e,I as o}from"./vue-8caab7e7.js";import"./lodash-cc4aae98.js";import"./naiveUI-06d71335.js";import"./ionicons5-13bb01b1.js";const t={},c={src:"/flower",class:"w-100% h-80vh"};function s(n,a){return e(),o("iframe",c)}const l=r(t,[["render",s]]);export{l as default};

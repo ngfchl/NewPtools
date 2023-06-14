6d158f9467f1abbbb5af3a9133d24654ed988f7c
ngfchl <ngfchl@126.com>
2023-06-14 08:40:11 +0800

    调整WS地址为动态

--- templates/assets/index-8d2eff3e.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-831a2fe3.js";import{H as e,I as o}from"./vue-3732522d.js";import"./lodash-18690875.js";import"./naiveUI-5e433394.js";import"./ionicons5-ced7f89a.js";const t={},c={src:"/flower",class:"w-100% h-80vh"};function s(n,a){return e(),o("iframe",c)}const l=r(t,[["render",s]]);export{l as default};
--- templates/assets/index-f9126f83.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-831a2fe3.js";import"./vue-3732522d.js";import"./lodash-18690875.js";import"./naiveUI-5e433394.js";import"./ionicons5-ced7f89a.js";const t={};function e(o,c){return" homepage "}const s=r(t,[["render",e]]);export{s as default};

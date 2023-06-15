35aaa1dc8db6fc500023f98e5f07f2fe4c36be32
DDSRem <1448139087@qq.com>
2023-06-15 19:07:57 +0800

    feat: optimize startup script

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,58 +17,78 @@ function WARN {
     echo -e "${WARN} ${1}"
 }
 
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+function token_verification {
 
-if [ -z "$TOKEN" ]; then
-  ERROR "Authorization: No TOKEN received. Exiting..."
-  exit 1
-fi
-# Get authorization response
-AUTH_RESPONSE=$(curl -s -G -d "token=$TOKEN&email=$DJANGO_SUPERUSER_EMAIL" http://api.ptools.fun/neice/check)
-INFO "$AUTH_RESPONSE"
-# Extract 'code' from the response
-AUTH_CODE=$(echo "$AUTH_RESPONSE" | jq -r '.code')
-INFO "$AUTH_CODE"
-if [ -z "$AUTH_CODE" ]; then
-  ERROR "Authorization: No AUTH_CODE received.  Exiting..."
-  exit 1
-elif [ "$AUTH_CODE" -ne 0 ]; then
-  ERROR "Authorization. Exiting..."
-  exit 1
-fi
+    if [ -z "$TOKEN" ]; then
+    ERROR "Authorization: No TOKEN received. Exiting..."
+    exit 1
+    fi
+    # Get authorization response
+    AUTH_RESPONSE=$(curl -s -G -d "token=$TOKEN&email=$DJANGO_SUPERUSER_EMAIL" http://api.ptools.fun/neice/check)
+    INFO "$AUTH_RESPONSE"
+    # Extract 'code' from the response
+    AUTH_CODE=$(echo "$AUTH_RESPONSE" | jq -r '.code')
+    INFO "$AUTH_CODE"
+    if [ -z "$AUTH_CODE" ]; then
+        ERROR "Authorization: No AUTH_CODE received.  Exiting..."
+        exit 1
+    elif [ "$AUTH_CODE" -ne 0 ]; then
+        ERROR "Authorization. Exiting..."
+        exit 1
+    fi
 
-if [ ! -f db/hosts ] || [ ! -f db/nowip_hosts.txt ]; then
-    INFO "HOSTS文件不存在，写入默认"
-    cp -n hosts/hosts db/hosts
-    cp -n hosts/nowip_hosts.txt db/nowip_hosts.txt
-else
-    INFO '存在自定义HOSTS文件，apply'
-fi
-if [ "$CloudFlareSpeedTest" = "true" ]; then
-    INFO "启动测速..."
-    bash cfst_hosts.sh
-else
-    INFO "跳过测速."
-fi
+}
 
-envsubst "\$DJANGO_WEB_PORT,\$WEBUI_PORT,\$FLOWER_UI_PORT" </ptools/nginx/nginx.conf >/etc/nginx/conf.d/default.conf
+function cloudflarespeedtest_host {
 
-for file in /ptools/supervisor/product/*.ini; do
-    sed -i "s/-l INFO/-l $LOGGER_LEVEL/g" "$file"
-done
-sed -i "s/--port 6379/--port $REDIS_SERVER_PORT/g" /ptools/supervisor/product/redis.ini
+    if [ ! -f db/hosts ] || [ ! -f db/nowip_hosts.txt ]; then
+        INFO "HOSTS文件不存在，写入默认"
+        cp -n hosts/hosts db/hosts
+        cp -n hosts/nowip_hosts.txt db/nowip_hosts.txt
+    else
+        INFO '存在自定义HOSTS文件，apply'
+    fi
+    if [ "$CloudFlareSpeedTest" = "true" ]; then
+        INFO "启动测速..."
+        bash cfst_hosts.sh
+    else
+        INFO "跳过测速."
+    fi
 
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    INFO "First container startup"
-    INFO "系统初始化中"
+}
+
+function first_start {
+
+    INFO "Nginx 初始化中..."
+    envsubst "\$DJANGO_WEB_PORT,\$WEBUI_PORT,\$FLOWER_UI_PORT" </ptools/nginx/nginx.conf >/etc/nginx/conf.d/default.conf
+
+    INFO "Supervisor 初始化中..."
+    for file in /ptools/supervisor/product/*.ini; do
+        sed -i "s/-l INFO/-l $LOGGER_LEVEL/g" "$file"
+    done
+    sed -i "s/--port 6379/--port $REDIS_SERVER_PORT/g" /ptools/supervisor/product/redis.ini
+
+    INFO "程序初始化中..."
     python3 manage.py migrate
-    touch $CONTAINER_ALREADY_STARTED
+    
     INFO "创建超级用户"
     DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME
     DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL
     DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
     python3 manage.py createsuperuser --noinput
     INFO "初始化完成"
+
+}
+
+token_verification
+
+cloudflarespeedtest_host
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    INFO "First container startup"
+    touch $CONTAINER_ALREADY_STARTED
+    first_start
 else
     INFO "Not first container startup"
 fi

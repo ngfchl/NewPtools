98008a104e9127015190c0506e8de0baa1efa6bc
ngfchl <328176843@qq.com>
2023-06-26 09:45:17 +0800

    update 优化启动脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,77 +8,81 @@ INFO="[${Green}INFO${Font}]"
 ERROR="[${Red}ERROR${Font}]"
 WARN="[${Yellow}WARN${Font}]"
 function INFO {
-    echo -e "${INFO} ${1}"
+  echo -e "${INFO} ${1}"
 }
 function ERROR {
-    echo -e "${ERROR} ${1}"
+  echo -e "${ERROR} ${1}"
 }
 function WARN {
-    echo -e "${WARN} ${1}"
+  echo -e "${WARN} ${1}"
 }
 
 function token_verification {
 
-    if [ -z "$TOKEN" ]; then
+  if [ -z "$TOKEN" ]; then
     ERROR "Authorization: No TOKEN received. Exiting..."
     exit 1
-    fi
-    # Get authorization response
-    AUTH_RESPONSE=$(curl -s -G -d "token=$TOKEN&email=$DJANGO_SUPERUSER_EMAIL" http://api.ptools.fun/neice/check)
-    INFO "$AUTH_RESPONSE"
-    # Extract 'code' from the response
-    AUTH_CODE=$(echo "$AUTH_RESPONSE" | jq -r '.code')
-    INFO "$AUTH_CODE"
-    if [ -z "$AUTH_CODE" ]; then
-        ERROR "Authorization: No AUTH_CODE received.  Exiting..."
-        exit 1
-    elif [ "$AUTH_CODE" -ne 0 ]; then
-        ERROR "Authorization. Exiting..."
-        exit 1
-    fi
+  fi
+  # Get authorization response
+  AUTH_RESPONSE=$(curl -s -G -d "token=$TOKEN&email=$DJANGO_SUPERUSER_EMAIL" http://api.ptools.fun/neice/check)
+  INFO "$AUTH_RESPONSE"
+  # Extract 'code' from the response
+  AUTH_CODE=$(echo "$AUTH_RESPONSE" | jq -r '.code')
+  INFO "$AUTH_CODE"
+  if [ -z "$AUTH_CODE" ]; then
+    ERROR "Authorization: No AUTH_CODE received.  Exiting..."
+    exit 1
+  elif [ "$AUTH_CODE" -ne 0 ]; then
+    ERROR "Authorization. Exiting..."
+    exit 1
+  fi
 
 }
 
 function cloudflarespeedtest_host {
 
-    if [ ! -f db/hosts ] || [ ! -f db/nowip_hosts.txt ]; then
-        INFO "HOSTS文件不存在，写入默认"
-        cp -n hosts/hosts db/hosts
-        cp -n hosts/nowip_hosts.txt db/nowip_hosts.txt
-    else
-        INFO '存在自定义HOSTS文件，apply'
-    fi
-    if [ "$CloudFlareSpeedTest" = "true" ]; then
-        INFO "启动测速..."
-        bash cfst_hosts.sh
-    else
-        INFO "跳过测速."
-    fi
+  if [ ! -f db/hosts ] || [ ! -f db/nowip_hosts.txt ]; then
+    INFO "HOSTS文件不存在，写入默认"
+    cp -n hosts/hosts db/hosts
+    cp -n hosts/nowip_hosts.txt db/nowip_hosts.txt
+  else
+    INFO '存在自定义HOSTS文件，apply'
+  fi
+  if [ "$CloudFlareSpeedTest" = "true" ]; then
+    INFO "启动测速..."
+    bash cfst_hosts.sh
+  else
+    INFO "跳过测速."
+  fi
 
 }
 
 function first_start {
 
-    INFO "Nginx 初始化中..."
-    envsubst "\$DJANGO_WEB_PORT,\$WEBUI_PORT,\$FLOWER_UI_PORT" </ptools/nginx/nginx.conf >/etc/nginx/conf.d/default.conf
+  INFO "程序初始化中..."
+  python3 manage.py migrate
 
-    INFO "Supervisor 初始化中..."
-    sed -i "s/:9001/:$SUPERVISOR_UI_PORT/g" /ptools/supervisor/prod.conf
-    for file in /ptools/supervisor/product/*.ini; do
-        sed -i "s/-l INFO/-l $LOGGER_LEVEL/g" "$file"
-    done
-    sed -i "s/--port 6379/--port $REDIS_SERVER_PORT/g" /ptools/supervisor/product/redis.ini
-    sed -i "s/--port=5566/--port=$FLOWER_UI_PORT/g" /ptools/supervisor/product/supervisor_celery_flower.ini
+  INFO "创建超级用户"
+  DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME
+  DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL
+  DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
+  python3 manage.py createsuperuser --noinput
+  INFO "初始化完成"
 
-    INFO "程序初始化中..."
-    python3 manage.py migrate
+}
 
-    INFO "创建超级用户"
-    DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME
-    DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL
-    DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
-    python3 manage.py createsuperuser --noinput
-    INFO "初始化完成"
+function init_supervisor() {
+
+  INFO "Nginx 初始化中..."
+  envsubst "\$DJANGO_WEB_PORT,\$WEBUI_PORT,\$FLOWER_UI_PORT" </ptools/nginx/nginx.conf >/etc/nginx/conf.d/default.conf
+
+  INFO "Supervisor 初始化中..."
+  sed -i "s/:9001/:$SUPERVISOR_UI_PORT/g" /ptools/supervisor/prod.conf
+  for file in /ptools/supervisor/product/*.ini; do
+    sed -i "s/-l INFO/-l $LOGGER_LEVEL/g" "$file"
+  done
+  sed -i "s/--port 6379/--port $REDIS_SERVER_PORT/g" /ptools/supervisor/product/redis.ini
+  sed -i "s/--port=5566/--port=$FLOWER_UI_PORT/g" /ptools/supervisor/product/supervisor_celery_flower.ini
 
 }
 
@@ -86,13 +90,15 @@ token_verification
 
 cloudflarespeedtest_host
 
+init_supervisor
+
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    INFO "First container startup"
-    touch $CONTAINER_ALREADY_STARTED
-    first_start
+  INFO "First container startup"
+  touch $CONTAINER_ALREADY_STARTED
+  first_start
 else
-    INFO "Not first container startup"
+  INFO "Not first container startup"
 fi
 
 INFO "启动服务"

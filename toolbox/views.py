9d7e0f351caff8cdca4a610a11eb7aca19bf146d
ngfchl <328176843@qq.com>
2023-07-23 11:04:41 +0800

    update 修复消息中包含>导致TG消息解析失败的BUG

--- toolbox/views.py
+++ toolbox/views.py
@@ -208,7 +208,7 @@ def send_text(message: str, title: str = '', url: str = None):
     notifies = parse_toml("notify")
     res = '你还没有配置通知参数哦！'
     try:
-        message = f'> {verify_token()}  \n\n{message}'
+        message = f'\> {verify_token()}  \n\n{message}'
         pass
     except Exception as e:
         msg = f'授权验证失败！'

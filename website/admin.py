eb870d788b045c6500b9f3eb0620872dc0269380
ngfchl <328176843@qq.com>
2023-09-16 20:37:56 +0800

    站点信息后台增加搜索框

--- website/admin.py
+++ website/admin.py
@@ -6,10 +6,15 @@ from .models import *
 # Register your models here.
 @admin.register(WebSite)
 class WebSiteAdmin(admin.ModelAdmin):
-    pass
+    list_filter = [
+        'name', 'nickname',
+    ]
+    list_display = [
+        'name', 'nickname', 'url', 'iyuu'
+    ]
+    search_fields = ['name']
 
 
 @admin.register(UserLevelRule)
 class UserLevelRuleAdmin(admin.ModelAdmin):
     pass
-

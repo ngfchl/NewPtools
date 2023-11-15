8cf3067ec70a8f2ac01c66a2f421ef0fabd450b9
ngfchl <328176843@qq.com>
2023-11-15 10:04:54 +0800

    聚合搜索完成所有站点搜索时关闭连接

--- ws/consumers.py
+++ ws/consumers.py
@@ -18,29 +18,29 @@ logger = logging.getLogger('ptools')
 class ProgressConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         try:
-            print("已连接")
+            logger.info("已连接")
             """
             """
             await self.accept()
 
         except Exception as e:
             # 输出异常信息
-            print(traceback.format_exc())
+            logger.error(traceback.format_exc())
 
     async def disconnect(self, close_code):
-        print("已断开")
+        logger.warning("已断开")
         pass
 
     async def receive(self, text_data):
+        logger.info(text_data)
         text_data_json = json.loads(text_data)
-        print(text_data)
         key = text_data_json['key']
         site_list = text_data_json['site_list']
         my_site_list = await self.get_my_site_list(site_list)  # Use the async function
         if len(my_site_list) <= 0:
             await self.send(text_data=json.dumps(CommonResponse.error(msg='没有站点可以搜索！').to_dict()))
         tasks = [search_and_parse_torrents(my_site, key) for my_site in my_site_list]
-
+        count = 0
         for completed_future in asyncio.as_completed(tasks):
             result = await completed_future
             if result.code == 0:
@@ -48,6 +48,9 @@ class ProgressConsumer(AsyncWebsocketConsumer):
             else:
                 logger.warning(result.msg)
             await self.send(text_data=json.dumps(result.to_dict()))
+            count += 1
+        if count == len(my_site_list):
+            await self.close()
 
     @database_sync_to_async
     def get_my_site_list(self, site_list):  # This is a synchronous function

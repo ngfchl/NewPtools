5afd1094c2bd947b199a41183f30148e63e42ee5
ngfchl <ngfchl@126.com>
2024-01-02 18:17:04 +0800

    添加工具方法：从字符串中提取文件大小

--- toolbox/tools.py
+++ toolbox/tools.py
@@ -1,7 +1,13 @@
 import ipaddress
+import re
 
 
 def is_valid_ip_address(ip_str):
+    """
+    判断字符串是否为ip地址
+    :param ip_str:
+    :return:
+    """
     try:
         ipaddress.IPv4Address(ip_str)
         return True
@@ -15,3 +21,24 @@ def is_valid_ip_address(ip_str):
         pass
 
     return False
+
+
+def extract_storage_size(input_string):
+    """
+    从字符串正则解析文件大小
+    :param input_string:
+    :return: ‘’MB，GB，TB，PB
+    """
+    # 定义正则表达式
+    pattern = re.compile(r'(\d+(\.\d+)?)\s+(B|KB|MB|GB|TB|PB)')
+
+    # 使用正则表达式进行匹配
+    match = pattern.search(input_string)
+
+    # 提取匹配的结果
+    if match:
+        value = float(match.group(1))
+        unit = match.group(3)
+        return f"{value} {unit}"
+    else:
+        return "0"
